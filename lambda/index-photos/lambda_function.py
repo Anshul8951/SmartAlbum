236cf6663a22e34a4800f07407a782decd13c17e
Anshul Agrawal <anshulagrawal@10-18-121-104.dynapool.wireless.nyu.edu>
2022-11-17 14:22:08 -0500

    Change

--- lambda/index-photos/lambda_function.py
+++ lambda/index-photos/lambda_function.py
@@ -5,7 +5,7 @@ import requests
 import base64
 #from requests_aws4auth import AWS4Auh
 
-#hey testing new changes for pipeline
+#hey testing new changes for pipeline again
 # credentials = boto3.Session().get_credentials()
 # awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
